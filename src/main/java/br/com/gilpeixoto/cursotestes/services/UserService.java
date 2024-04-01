020b99d5e50f9e692a03851021b91fa939c405fe
Gil-Peixoto <araujo.gil27@gmail.com>
2024-04-01 16:33:02 -0300

    Inteface User Service

---
+++ src/main/java/br/com/gilpeixoto/cursotestes/services/UserService.java
@@ -0,0 +1,11 @@
+package br.com.gilpeixoto.cursotestes.services;
+
+import br.com.gilpeixoto.cursotestes.domain.User;
+
+public interface UserService {
+
+    User findById(Integer id);
+
+    
+
+}
