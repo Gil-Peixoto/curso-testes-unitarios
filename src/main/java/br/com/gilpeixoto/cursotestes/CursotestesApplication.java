635d33a32bf99f66609ec766a20807371b05bc9c
Gil-Peixoto <araujo.gil27@gmail.com>
2024-04-01 14:28:26 -0300

    projeto iniciado

---
+++ src/main/java/br/com/gilpeixoto/cursotestes/CursotestesApplication.java
@@ -0,0 +1,14 @@
+package br.com.gilpeixoto.cursotestes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursotestesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursotestesApplication.class, args);
+		System.out.println("Olá mundo");
+	}
+
+}
---
+++ src/test/java/br/com/gilpeixoto/cursotestes/CursotestesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gilpeixoto.cursotestes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursotestesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
