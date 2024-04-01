17fa831247e58ff71293b0190e7c66705797afce
Gil-Peixoto <araujo.gil27@gmail.com>
2024-04-01 14:49:12 -0300

    classe user

--- src/main/java/br/com/gilpeixoto/cursotestes/CursotestesApplication.java
+++ src/main/java/br/com/gilpeixoto/cursotestes/CursotestesApplication.java
@@ -3,12 +3,17 @@ package br.com.gilpeixoto.cursotestes;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.gilpeixoto.cursotestes.domain.User;
+
 @SpringBootApplication
 public class CursotestesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursotestesApplication.class, args);
-		System.out.println("Olá mundo");
+
+		User usr = new User(1, "gil", "araujo.gil27@gmail.com", "123456");
+
+		usr.toString();
 	}
 
 }
---
+++ src/main/java/br/com/gilpeixoto/cursotestes/domain/User.java
@@ -0,0 +1,27 @@
+package br.com.gilpeixoto.cursotestes.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+    private String password;
+
+}
