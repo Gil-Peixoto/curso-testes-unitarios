059e416938411ba54eba90452a2149a2a51aca13
Gil-Peixoto <araujo.gil27@gmail.com>
2024-04-01 19:30:35 -0300

    Perfil de testes

--- src/main/java/br/com/gilpeixoto/cursotestes/domain/User.java
+++ src/main/java/br/com/gilpeixoto/cursotestes/domain/User.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,10 +14,12 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
+@Table(name="tb_user")
 public class User {
 
-    @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    @Id
     private Integer id;
     private String name;
 
