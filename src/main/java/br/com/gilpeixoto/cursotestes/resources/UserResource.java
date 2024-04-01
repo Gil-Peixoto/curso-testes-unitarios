00ee531b8848a851d39414b7cbfba3c68bf1adb7
Gil-Peixoto <araujo.gil27@gmail.com>
2024-04-01 16:34:12 -0300

    Classe User Resource

--- src/main/java/br/com/gilpeixoto/cursotestes/resources/UserResource.java
+++ src/main/java/br/com/gilpeixoto/cursotestes/resources/UserResource.java
@@ -1,5 +1,6 @@
 package br.com.gilpeixoto.cursotestes.resources;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -7,15 +8,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.gilpeixoto.cursotestes.domain.User;
+import br.com.gilpeixoto.cursotestes.services.UserService;
 
 @RestController
 @RequestMapping(value = "/user")
 public class UserResource {
 
+    @Autowired
+    private UserService service;
+
     @GetMapping(value = "/{id}")
     public ResponseEntity<User> findById(@PathVariable Integer id) {
 
-        return ResponseEntity.ok().body(new User(1, "gil", "araujo.gil27@gmail.com", "123456")) ;
+        return ResponseEntity.ok().body(service.findById(id));
 
     }
 
