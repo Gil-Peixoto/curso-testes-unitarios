69c19e0248257f7ecd3fc5ce7b90fa65707e53fd
Gil-Peixoto <araujo.gil27@gmail.com>
2024-04-03 18:33:39 -0300

    Criado endpoint findAll para User

--- src/main/java/br/com/gilpeixoto/cursotestes/resources/UserResource.java
+++ src/main/java/br/com/gilpeixoto/cursotestes/resources/UserResource.java
@@ -1,5 +1,8 @@
 package br.com.gilpeixoto.cursotestes.resources;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -23,9 +26,12 @@ public class UserResource {
 
     @GetMapping(value = "/{id}")
     public ResponseEntity<UserDTO> findById(@PathVariable Integer id) {
-
         return ResponseEntity.ok().body(mapper.map(service.findById(id), UserDTO.class));
+    }
 
+    @GetMapping
+    public ResponseEntity<List<UserDTO>> findAll() {
+        return ResponseEntity.ok().body(service.findAll().stream().map(x -> mapper.map(x, UserDTO.class)).collect(Collectors.toList()));
     }
 
 }
--- src/main/java/br/com/gilpeixoto/cursotestes/services/UserService.java
+++ src/main/java/br/com/gilpeixoto/cursotestes/services/UserService.java
@@ -1,10 +1,14 @@
 package br.com.gilpeixoto.cursotestes.services;
 
+import java.util.List;
+
 import br.com.gilpeixoto.cursotestes.domain.User;
 
 public interface UserService {
 
     User findById(Integer id);
+
+    List<User> findAll();
 
     
 
